7c9395660ded738ac3e6ae87e00c568583b2369b
juan jose suarez <jjsuarezf@ufpso.edu.co>
2025-03-08 07:56:49 -0500

    Quiz

---
+++ src/Ejercicio.java
@@ -0,0 +1,11 @@
+public class Ejercicio {
+        public static void main(String[] args) throws Exception {
+            System.out.println("ingrese un numero entero");
+         int=i+1;
+        byte  num1=scanner.nextbyte;
+        byte while (num1+i);
+        System.out.println(num+i);
+        }
+    }
+    
+
